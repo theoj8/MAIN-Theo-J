449b01d8f0e88657e91443b6f8559cba6409c7e5
Theo Jadeau <jadeautheo8@gmail.com>
2026-07-28 14:34:47 +0200

    mise en place

---
+++ site portfolio architecte/backend/models/categories.model.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+	const Categories = sequelize.define(
+		"categories",
+		{
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			},
+		},
+		{timestamps: false}
+	)
+	return Categories
+}
